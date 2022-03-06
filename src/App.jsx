f52b9b04e8a6fb8ed9aba2e3dd4a318ed460cf7c
mohammedrakib944 <mohammedrakib944@gmail.com>
2022-03-06 21:23:28 +0600

    portfolio project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App() {
+  return (
+    <div>
+      <Home />
+      <h1>Vite Project</h1>
+    </div>
+  );
+}
