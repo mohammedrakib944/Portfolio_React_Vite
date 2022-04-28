a2251972f663ce0c8132dd00ef70853649011981
mohammedrakib944 <mohammedrakib944@gmail.com>
2022-04-28 17:55:05 +0600

    Settings added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,6 +7,7 @@ import "./nav.css";
 
 export default function Nav() {
   const [toggleButton, setToggleButton] = useState("hideNav");
+  const [toggleSetting, setToggleSetting] = useState("");
 
   window.onscroll = function () {
     const navi = document.querySelector(".navSection");
@@ -15,47 +16,89 @@ export default function Nav() {
     else navi.classList.remove("secondNav");
   };
 
+  function changePrimaryColor(color) {
+    document.documentElement.style.setProperty("--primaryColor", color);
+  }
+
   return (
-    <div className="navSection">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-md-4">
-            <div className="menubar">
-              <button
-                className="hideMenu"
-                onClick={() => setToggleButton("showNav")}
-              >
-                <i class="fa-solid fa-bars"></i>
-              </button>
-              <NavLink to="/">
-                <h2 className="Logo">
-                  Md. <strong> Rakib</strong>
-                </h2>
-              </NavLink>
-            </div>
+    <>
+      <div className="navSection">
+        <div className="settingsSilder">
+          <button onClick={() => setToggleSetting("showSetting")}>
+            <i class="fa-solid fa-screwdriver-wrench"></i>
+          </button>
+        </div>
+
+        <div className={`settings ${toggleSetting}`}>
+          <button className="hideSetting" onClick={() => setToggleSetting("")}>
+            <i class="fa-solid fa-angles-left"></i>
+          </button>
+          <h5 className="settingsTitle">Settings</h5>
+          <hr />
+          <h6 className="colorTitle">Primary Color</h6>
+          <div className="primaryColorChage">
+            <button
+              className="colorBtn redBg"
+              onClick={() => changePrimaryColor("#f60073")}
+            ></button>
+            <button
+              className="colorBtn greenBg"
+              onClick={() => changePrimaryColor("#b9ff00")}
+            ></button>
+            <button
+              className="colorBtn blueBg"
+              onClick={() => changePrimaryColor("#00c0ff")}
+            ></button>
+            <button
+              className="colorBtn purpleBg"
+              onClick={() => changePrimaryColor("#9d44f5")}
+            ></button>
+            <button
+              className="colorBtn yellowBg"
+              onClick={() => changePrimaryColor("#f1c40f")}
+            ></button>
           </div>
-          <div className="col-md-4">
-            <ul className={`navigation ${toggleButton}`}>
-              <span
-                className="hideNavigation"
-                onClick={() => setToggleButton("hideNav")}
-              ></span>
-              <li>
-                <NavLink to="/">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/partners">Partnars</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-            </ul>
+        </div>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-4">
+              <div className="menubar">
+                <button
+                  className="hideMenu"
+                  onClick={() => setToggleButton("showNav")}
+                >
+                  <i class="fa-solid fa-bars"></i>
+                </button>
+                <NavLink to="/">
+                  <h2 className="Logo">
+                    Md. <strong> Rakib</strong>
+                  </h2>
+                </NavLink>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <ul className={`navigation ${toggleButton}`}>
+                <span
+                  className="hideNavigation"
+                  onClick={() => setToggleButton("hideNav")}
+                ></span>
+                <li>
+                  <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/partners">Partnars</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/contact">Contact</NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
